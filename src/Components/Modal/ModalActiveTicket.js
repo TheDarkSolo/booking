0a1229146c590439a686fb863615c0c2aefa2235
Gaiyev19 <Gaiyev19>
2023-06-06 13:06:18 +0000

    r

--- src/Components/Modal/ModalActiveTicket.js
+++ src/Components/Modal/ModalActiveTicket.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
 export default function ModalActiveTicket({ hasActiveTicket, setHasActiveTicket }) {
-
+    const { t } = useTranslation()
     const handleClose = () => setHasActiveTicket(false);
     return (
         <Modal show={hasActiveTicket} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>Ошибка</Modal.Title>
+                <Modal.Title>{t("error")}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 {" "}
-                У вас уже есть активный талон. Данные были высланы в виде SMS. Проверьте свои сообщения.
+                {/* У вас уже есть активный талон. Данные были высланы в виде SMS. Проверьте свои сообщения. */}
+                {t("hasActiveTicket")}
             </Modal.Body>
             <Modal.Footer>
                 <Link to="/reservation">
